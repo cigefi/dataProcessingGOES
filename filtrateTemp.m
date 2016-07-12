eb7ec6f83817693d2e07ed358e292d52158622e9
Roberto Villegas-Diaz <bthillo@hotmail.com>
2016-07-12 13:08:32 -0600

    Filter function added

---
+++ filtrateTemp.m
@@ -0,0 +1,41 @@
+function[data,data2] = filtrateTemp(data,temp)
+    data(data<(temp-1)) = 0;
+    data(data>(temp+1)) = 0;
+    for i=1:length(data(:,1))
+        for j=1:length(data(1,:))
+            temp = data(:,:);
+            if data(i,j)~=0
+                fr = i;
+                lr = i;
+                fc = j;
+                lc = j;
+                if i+1 <= length(data(:,1))
+                    lr = i+1;
+                end
+                if j+1 <= length(data(1,:))
+                    lc = j+1;
+                end
+                if i-1>=1
+                    fr = i-1;
+                end
+                if j-1>=1
+                    fc = j-1;
+                end
+                temp(i,j) = 0;
+                cof = temp(fr:lr,fc:lc);
+                if ~hasNeighbors(cof)%~hasNeighbors(cof)
+                    %data2(i,j) = data(i,j);
+                    data2(i,j) = 0;
+                end
+            end
+        end
+    end
+end
+
+function [res] = hasNeighbors(data)
+    if sum(data~=0)>0
+        res = 1;
+    else
+        res = 0;
+    end
+end
