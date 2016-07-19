ce431227db5373dbb707c2c80992bd335e0aff5c
Centro de Investigaciones Geofísicas <roberto.villegas@ucr.ac.cr>
2016-07-19 14:48:40 -0600

    Minor changes

--- GOES.m
+++ GOES.m
@@ -14,20 +14,33 @@ function [] = GOES(dirName)
     end
     [IR4,VIS,WV,tlIR4,tlVIS,tlWV] = dataProcessingIR4(dirName);
     if ~isempty(IR4)
-        save(char(savePath.concat('IR4.mat')),'IR4','-v7.3');
+        n = getFilesCount(savePath,'IR4');
+        save(char(savePath.concat(strcat('IR4-',num2str(n),'.mat'))),'IR4','-v7.3');
+        %save(char(savePath.concat('IR4.mat')),'IR4','-v7.3');
     end
     if ~isempty(tlIR4)
-        save(char(savePath.concat('IR4-timeline.mat')),'tlIR4');
+        n = getFilesCount(savePath,'tlIR4');
+        save(char(savePath.concat(strcat('tlIR4-',num2str(n),'.mat'))),'tlIR4','-v7.3');
+        %save(char(savePath.concat('IR4-timeline.mat')),'tlIR4');
     end
     if ~isempty(VIS)
-        save(char(savePath.concat('VIS.mat')),'VIS','-v7.3');
+        n = getFilesCount(savePath,'VIS');
+        save(char(savePath.concat(strcat('VIS-',num2str(n),'.mat'))),'VIS','-v7.3');
+        %save(char(savePath.concat('VIS.mat')),'VIS','-v7.3');
     end
     if ~isempty(tlVIS)
-        save(char(savePath.concat('VIS-timeline.mat')),'tlVIS');
+        n = getFilesCount(savePath,'tlVIS');
+        save(char(savePath.concat(strcat('tlVIS-',num2str(n),'.mat'))),'tlVIS','-v7.3');
+        %save(char(savePath.concat('VIS-timeline.mat')),'tlVIS');
     end
     if ~isempty(WV)
-        save(char(savePath.concat('WV.mat')),'WV','-v7.3');
+        n = getFilesCount(savePath,'WV');
+        save(char(savePath.concat(strcat('WV-',num2str(n),'.mat'))),'WV','-v7.3');
+        %save(char(savePath.concat('WV.mat')),'WV','-v7.3');
     end
     if ~isempty(tlWV)
-        save(char(savePath.concat('WV-timeline.mat')),'tlWV');
+        n = getFilesCount(savePath,'tlWV');
+        save(char(savePath.concat(strcat('tlWV-',num2str(n),'.mat'))),'tlWV','-v7.3');
+        %save(char(savePath.concat('WV-timeline.mat')),'tlWV');
     end
+end
